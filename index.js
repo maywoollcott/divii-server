a262549c1054b821d7f1a7bd8a3d4c57d8233772
May Woollcott <mwoollcott@better.com>
2021-12-24 11:56:48 -0500

    heroku

--- index.js
+++ index.js
@@ -27,15 +27,11 @@ mongoose.connect(
   { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false },
   (err) => {
     if (err) {
-      console.log(
-        `Eek, something went wrong when connecting to the database: ${err}`
-      );
+      console.log(`Eek, something went wrong when connecting to the database: ${err}`);
     } else {
       console.log('Yippee! Connected to the database.');
     }
   }
 );
 
-app.listen(3002, () =>
-  console.log(`Server is running on port 3002! ${process.env.ATLAS_PASSWORD}`)
-);
+app.listen(3002, () => console.log(`Server is running on port 3002! ${process.env.ATLAS_PASSWORD}`));
